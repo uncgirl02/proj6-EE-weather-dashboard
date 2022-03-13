4348b057303c4d4e3736934a782e3ce0fb9c5ee1
Erin Ewart <ems1980@hotmail.com>
2022-03-13 18:23:42 -0400

    fixed function to display 5 Day Forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ var userFormEl = document.querySelector("#user-form")
 var weatherContainerEl = document.querySelector('#city-weather')
 var searchButton = document.querySelector('#btn')
 var currentWeatherContainter = document.querySelector("#current-weather")
-var forcastContainer = document.querySelector("#forecast")
+var forecastContainer = document.querySelector("#forecast")
 var cityName = ""
 var currentCity = document.querySelector("#city-search-term");
 var currentDate = document.querySelector("#current-date")
@@ -71,31 +71,30 @@ fetch(uvIndexUrl).then(function (response) {
   
   //Function to display current weather
   function currentWeather(weather) {
-    
-    console.log(cityName);
-    console.log(currentCity);
     currentCity.innerText = cityName
-  
-    
+    currentCity.className = "fw-bold fs-1 p-3"
     currentDate.innerText = `(${moment(weather.list.dt_txt).format("l")})`;
- 
-  
+    currentDate.className = "fw-bold fs-1 p-3"
+
+    currentWeatherContainter.className = "border border-secondary rounded-2 ms-3"
+    
     var icon = weather.list[0].weather[0].icon;
     var iconUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
     console.log(currentIcon);
     currentIcon.setAttribute("src", iconUrl);
-  
-    
     
     var currentTemp = document.createElement("div");
+    currentTemp.className = "p-3"
     currentTemp.innerText = "Temp: " + weather.list[0].main.temp + "°F";
     subTitle.append(currentTemp);
 
     var currentWind = document.createElement("div");
+    currentWind.className = "pt-3 pb-3"
     currentWind.innerText = "Wind Speed: " + weather.list[0].wind.speed + "mph";
     currentTemp.append(currentWind);
     
     currentHumidity.innerText = "Humidity: " + weather.list[0].main.humidity + "%";
+    currentHumidity.className = "pt-3 pb-3"
     currentWind.append(currentHumidity)
   
   
@@ -108,53 +107,72 @@ fetch(uvIndexUrl).then(function (response) {
   //Function to Display UV Index
   function currentUvIndex(uvi) {
     var currentUVI = document.createElement("div")
+    currentUVI.className = "pt-3 pb-3"
     currentHumidity.append(currentUVI)
+    currentUVI.innerText = "UV Index: ";
+    
+    var uviColor = document.createElement("span")
+    currentUVI.append(uviColor)
     var uviIndex = uvi.current.uvi;
-    currentUVI.innerText = uviIndex;
-  
+    uviColor.innerText = uviIndex
+    
     if (uviIndex <= 2) {
-      currentUVI.className = "text-white p-1 bg-success";
-    } else if (uviIndex > 2 && uviIndex <= 7) {
-      currentUVI.className = "text-white p-1 bg-warning";
+      uviColor.className = "p-1 bg-success rounded";
+    } else if (uviIndex > 2 && uviIndex <= 5) {
+      uviColor.className = "p-1 bg-warning rounded";
+    } else if (uviIndex > 5 && uviIndex <= 7) {
+      uviColor.className = "p-1 bg-orange rounded";
     } else if (uviIndex > 7) {
-      currentUVI.className = "text-white p-1 bg-danger";
+      uviColor.className = "p-1 bg-danger rounded";
     }
   }
 
-  //Function to Display the 5 Day future Forcast
+  //Function to display the 5 Day future Forcast
   function futureForecast(forecast) {
+
+    var futureHeader = document.createElement("h3")
+    futureHeader.innerText = "5-Day Forecast:"
+    console.log(futureHeader);
+    forecastContainer.appendChild(futureHeader)
+    forecastContainer.classname = "d-flex flex-column ms-3"
 
     var dailyForecast = forecast.daily;
     for (var i = 1; i < 6; i++) {
       var nextDay = dailyForecast[i];
-      var forcastedDate = moment(nextDay.dt * 1000).format("l");
+      var forecastedDate = moment(nextDay.dt * 1000).format("l");
   
-      var dayWeather = document.createElement("div").className = "card d-flex bg-navy text-white p-3 col-8 col-xl-2 col-md-4 col-sm-4 m-3";
-      forcastContainer.appendChild(dayWeather);
+      var dayWeather = document.createElement("div")
+      dayWeather.className = "weather-card card d-flex text-white p-3 col-8 col-xl-2 col-md-4 col-sm-4 m-3";
+      forecastContainer.appendChild(dayWeather);
   
-      var weatherDate = document.createElement("h5").className = "d-flex justify-content-center";
-      weatherDate.innerText = forcastedDate;
+      var weatherDate = document.createElement("h5")
+      weatherDate.className = "d-flex justify-content-center";
+      weatherDate.innerText = forecastedDate;
       dayWeather.appendChild(weatherDate);
   
-      var forecastIcon = nextDay.weather[0].icon;
+      var forecastIcon = nextDay.weather.icon;
+      console.log(forecastIcon);
       var forecastIconUrl = "http://openweathermap.org/img/wn/" + forecastIcon + "@2x.png";
-      var weatherIcon = document.createElement("img").attr("src", forecastIconUrl);
+      var weatherIcon = document.createElement("img").setAttribute("src", forecastIconUrl);
       dayWeather.append(weatherIcon);
   
-      var maxTemp = document.createElement("div").add("text-start");
-      maxTemp.innerText = "High Temp:" + nextDay.temp.max + "°F";
+      var maxTemp = document.createElement("div")
+      maxTemp.className = "text-start";
+      maxTemp.innerText = "High Temp: " + nextDay.temp.max + "°F";
       dayWeather.append(maxTemp);
   
-      var minTemp = document.createElement("div").add("text-start");
-      minTemp.innerText = "Low Temp:" + nextDay.temp.min + "°F";
+      var minTemp = document.createElement("div")
+      minTemp.className = "text-start";
+      minTemp.innerText = "Low Temp: " + nextDay.temp.min + "°F";
       dayWeather.append(minTemp);
   
-      var dayHumidity = document.createElement("div").add("text-start");
-      dayHumidity.innerText = "Humidity:" + nextDay.humidity;
+      var dayHumidity = document.createElement("div")
+      dayHumidity.className = "text-start";
+      dayHumidity.innerText = "Humidity: " + nextDay.humidity + "%";
       dayWeather.append(dayHumidity);
     }
   }
 
-// add event listeners to forms
+// add event listener to forms
 userFormEl.addEventListener('submit', formSubmitHandler);
 
